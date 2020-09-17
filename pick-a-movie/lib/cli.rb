94093b981772f50c37b3f1aeeff370ac484eb3f1
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-17 23:04:11 +0000

    Defining cli methods

--- pick-a-movie/lib/cli.rb
+++ pick-a-movie/lib/cli.rb
@@ -5,7 +5,26 @@ require 'nokogiri'
 class CLI
   
   def run
-    puts "Hello there!"
+    puts "Hi there! We'll pick a movie for you, just tell us which list you prefer!"
+  end
+  
+  def greeting
+    
+    puts "Enter '"
+  end
+  
+  def menu
+    
+  end
+  
+  def pick_a_movie
+    
+    
+    
+  end
+  
+  def bye
+    puts "Enjoy your movie!"
   end
   
 end
