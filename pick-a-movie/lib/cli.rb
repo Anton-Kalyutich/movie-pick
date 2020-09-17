35fafa7e3a9a2730269b66242da06b967af7de1e
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-17 21:55:19 +0000

    Finished tailoring a basic skeleton of the project

---
+++ pick-a-movie/lib/cli.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/scraper.rb"
+require_relative "../lib/movie.rb"
+require 'nokogiri'
+
+class CLI
+  
+  def run
+    puts "Hello there!"
+  end
+  
+end
--- pick-a-movie/lib/pick/a/movie/version.rb
+++
@@ -1,7 +0,0 @@
-module Pick
-  module A
-    module Movie
-      VERSION = "0.1.0"
-    end
-  end
-end
