15773032472eaf436010e41066b10b755291d9a4
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-18 23:38:39 +0000

    Introduced #menu cli method, erased scraper class to start from scratch

--- pick-a-movie/lib/cli.rb
+++ pick-a-movie/lib/cli.rb
@@ -7,31 +7,51 @@ class CLI
   def run
     greeting
     list_movies
-    
+    menu
+    bye
   end
   
   def greeting
-    puts "Hi there! We'll make a random choice of 3 movies for you!"
+    #greets the user and tells him/her the program works
+    puts "Hi there! We'll pick 3 random movies for you!"
     puts "Press 'enter' when you're ready."
-    if gets.chomp == "\n"
-      #just moving on to the next method here
-    end
-  end
-  
-  def menu
-    
+    gets
   end
   
   def list_movies
+    #lists 3 movies randomly selected frm the corrsponding websites
     puts "Here you go:"
-    puts "1. Lady Bird (2017) - 99% on 'Rotten Tomatoes'"
-    puts "2. God Father (1972) - 9,2 on 'IMDb'"
-    puts "3. "
+    puts "1. Lady Bird (2017) - 99% on 'Rotten Tomatoes'."
+    puts "2. God Father (1972) - 9,2 on 'IMDb'."
+    puts "3. Citizen Kane (1941) - 100% on 'Metacritic'."
+  end
+  
+  def menu
+    input = nil
+    while input != 'q'
+    #gotta fix weird 'q' behaviour later
+      puts "\nEnter '1', '2', or '3' to learn more about a movie (enter 'q' to exit):"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on movie 1..."
+      when "2"
+        puts "More info on movie 2..."
+      when "3"
+        puts "More info on movie 3..."
+      when 'q'
+        break
+      else
+        puts "Sorry, I didn't get it."
+        sleep 1
+        menu
+      end
+    end
   end
     
   
   def bye
-    puts "Enjoy your movie!"
+    puts "\nEnjoy the movie!"
   end
   
 end
--- pick-a-movie/lib/scraper.rb
+++ pick-a-movie/lib/scraper.rb
@@ -1,25 +1,9 @@
 require 'open-uri'
-require 'pry'
 require 'nokogiri'
 
 class MovieScraper
   
-  def self.scrape_rotten_tomatoes
-    array_of_movies = []
-    html = open(https://www.rottentomatoes.com/top/bestofrt/)
-    doc = Nokogiri::HTML(html)
-    movies = doc.css("a.unstyled.articleLink")
-    movies.each do |movies|
-      
-      
-      hash = {}
-      hash[:title] = movie.css("").text
-      hash[:] = student.css("p.student-location").text
-      hash[:profile_url] = student.css("a").attribute("href").value
-      array_of_movies << hash
-    end
-    array_of_movies
+  
     
-  end
 end
 
