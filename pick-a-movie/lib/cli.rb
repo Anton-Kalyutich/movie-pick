d30e62d441f25d0dc860ea893c642f9780b4d604
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-18 22:28:49 +0000

    Added #greeting method

--- pick-a-movie/lib/cli.rb
+++ pick-a-movie/lib/cli.rb
@@ -5,23 +5,30 @@ require 'nokogiri'
 class CLI
   
   def run
-    puts "Hi there! We'll pick a movie for you, just tell us which list you prefer!"
+    greeting
+    list_movies
+    
   end
   
   def greeting
-    
-    puts "Enter '"
+    puts "Hi there! We'll make a random choice of 3 movies for you!"
+    puts "Press 'enter' when you're ready."
+    if gets.chomp == "\n"
+      #just moving on to the next method here
+    end
   end
   
   def menu
     
   end
   
-  def pick_a_movie
-    
-    
-    
+  def list_movies
+    puts "Here you go:"
+    puts "1. Lady Bird (2017) - 99% on 'Rotten Tomatoes'"
+    puts "2. God Father (1972) - 9,2 on 'IMDb'"
+    puts "3. "
   end
+    
   
   def bye
     puts "Enjoy your movie!"
