0bae0d3932595e00c0c20d1106bf355702faca89
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-21 03:32:43 +0000

    Refactored #menu methodd

--- pick-a-movie/lib/cli.rb
+++ pick-a-movie/lib/cli.rb
@@ -18,8 +18,7 @@ class CLI
   
   def list_movies
     puts "\nHere you go:"
-    @movies = Movie.all
-    @movies.each_with_index do |movie, i|
+    Movie.all.each_with_index do |movie, i|
       puts "#{i+1}. #{movie.title} (#{movie.year})"
     end
   end
@@ -29,10 +28,11 @@ class CLI
     until input == 'done'
       puts "\nEnter '1', '2', or '3' for more info on a movie (or enter 'list' or 'done'):"
       input = gets.strip.downcase
+      i = input.to_i
+      movies = Movie.all
       
-      
-      if input.to_i > 0 && input.to_i < 4
-        the_movie = @movies[input.to_i-1]
+      if i > 0 && i < movies.size
+        the_movie = movies[i-1]
         puts "\nTitle: '#{the_movie.title}'"
         puts "Released: #{the_movie.year}"
         puts "Genre: #{the_movie.genre}"
