5ae180f284808e4c5b69ea425546f265fe93f1a4
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-21 11:47:23 +0000

    Scraper now returns a hash with info, rather than both scraping and instantiating Movie instances

--- pick-a-movie/lib/scraper.rb
+++ pick-a-movie/lib/scraper.rb
@@ -7,24 +7,26 @@ class Scraper
   end
   
   def self.scrape_suggestmemovie
-    #Goes to suggestmemovie.com, scrapes properties, instantiates a Movie and saves it
+    #Goes to suggestmemovie.com, scrapes properties and returns a hash with all the info
     doc = Nokogiri::HTML(open("https://suggestmemovie.com/"))
-    movie = Movie.new
+    movie_info = {}
     
-    movie_info = doc.css("div.col-sm-12 div.card.my-3:first-of-type")
-    title_plus_year = movie_info.css("div.card-header").text.strip
+    info = doc.css("div.col-sm-12 div.card.my-3:first-of-type")
+    title_plus_year = info.css("div.card-header").text.strip
     title_plus_year_array = title_plus_year.split
-    movie.year = title_plus_year_array.pop[1...-1]
-    movie.title = title_plus_year_array.join(" ")
+    movie_info[:year] = title_plus_year_array.pop[1...-1]
+    movie_info[:title] = title_plus_year_array.join(" ")
     
-    movie.plot = movie_info.css("p.card-text").text.strip
-    movie.genre = movie_info.css("dl.row dd.col-sm-9:first-of-type").text
-    movie.language = movie_info.css("dl.row dd.col-sm-9:nth-of-type(2)")
-    director = movie_info.css("dl.row dd.col-sm-9:nth-of-type(3)").text.strip.gsub(/\s+/, " ")
-    movie.director = director.split(" , ").join(", ")
+    movie_info[:plot] = movie_info.css("p.card-text").text.strip
+    movie_info[:genre] = movie_info.css("dl.row dd.col-sm-9:first-of-type").text
+    movie_info[:language] = movie_info.css("dl.row dd.col-sm-9:nth-of-type(2)")
+    director = info.css("dl.row dd.col-sm-9:nth-of-type(3)").text.strip.gsub(/\s+/, " ")
+    movie_info[:director] = director.split(" , ").join(", ")
     cast = movie_info.css("dl.row dd.col-sm-9:nth-of-type(4)").text.strip.gsub(/\s+/, " ")
-    movie.cast = cast.split(" , ").join(", ")
-    movie.score = doc.css("div.card-body.text-center.my-3 h1").text
+    movie_info[:cast] = cast.split(" , ").join(", ")
+    movie_info[:score] = doc.css("div.card-body.text-center.my-3 h1").text
+    
+    movie_info
   end
   
 end
